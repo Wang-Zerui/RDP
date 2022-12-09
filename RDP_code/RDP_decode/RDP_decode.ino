e9f38adf98e3184d7ad03c896a8b757e46d86845
sabkx <sabkx1433223.yg@gmail.com>
2022-12-09 16:52:43 +0800

    update

--- RDP_code/RDP_decode/RDP_decode.ino
+++ RDP_code/RDP_decode/RDP_decode.ino
@@ -19,17 +19,17 @@ void setup() {
   pinMode(A0,INPUT);
   pinMode(A2,OUTPUT);
   // Serial.println("hi3");
-  cli();
+  // cli();
   TCCR2A = (1 << WGM21);
   TCCR2B = (1 << CS21)|(1 << CS20);
   TIMSK2 = (1 << OCIE2A);
   OCR2A = 20; //define upper limit of counter before it resets.
   ADMUX=0x40;
-  ADCSRA|=B11000111;
+  ADCSRA=B11000111;
   // ADCSRA |= B00000100; // set ADIE bit in ADCSRA register
-  ADCSRA |= B01000000; // set ADSC bit in ADCSRA register
+  // ADCSRA |= B01000000; // set ADSC bit in ADCSRA register
   // Serial.println("HI");
-  sei();
+  // sei();
 }
 // int arr[MAXN][10];
 // int buff[MAXN<<5];
@@ -43,59 +43,64 @@ void loop() {
   // TIMSK2 = (1 << OCIE2A);
   // OCR2A = 20; //define upper limit of counter before it resets.
   // sei();
+  // digitalWrite(8,1);
+  // PORTB^=0x01;
 }
 ISR(TIMER2_COMPA_vect){
   // Serial.println("hi");// for testing
-  PORTC^=0x04;
-  // r++;
-  // // Serial.println("h2");
-  // r&=(7);
-  // // Serial.println("h3");
-  // buff[r]=ADC;
-  // // ADCSRA|=0x40;
-  // if(buff[r]>512){
-  //   PORTC|=0x04;
-  // }else{
-  //   PORTC&=~0x04;
-  // }
-  // // ADCSRA|=0x40;
-  // // Serial.println("h4");
-  // // for(int i=0;i<=7;i++){
-  // //   Serial.println("h5");
-  // //   Serial.println(buff[i]);
-  // // }
+  // PORTC^=0x04;
+  r++;
+  // Serial.println("h2");
+  r&=(7);
+  // Serial.println("h3");
+  buff[r]=ADC;
+  ADCSRA|=0x40;
+  if(buff[r]>512){//this line works have signal in blue
+    PORTC|=0x04;
+  }else{
+    PORTC&=~0x04;
+  }
   // ADCSRA|=0x40;
-  // vals[0]=buff[(2)&7]+(buff[(3)&7]<<1)+buff[(4)&7]-buff[(6)&7]-(buff[(7)&7]<<1)-buff[0];
-  // vals[1]=(buff[(2)&7]<<1)-(buff[(4)&7]<<1)+(buff[(6)&7]<<1)-(buff[0]<<1);
-  // vals[2]=(buff[(1)&7]<<1)+(buff[(2)&7])-buff[(4)&7]-(buff[(5)&7]<<1)-(buff[(6)&7])+buff[0];
-  // vals[3]=-(buff[(2)&7]<<1)+(buff[(4)&7]<<1)-(buff[(6)&7]<<1)+(buff[0]<<1);
-  // long freq_1000=vals[0]*vals[0]+vals[2]*vals[2];//is this correct?
-  // long freq_2000=vals[1]*vals[1]+vals[3]*vals[3];
-  // if(freq_1000>freq_2000){
-  //   //then it is 1000 Hz
-  //   // Serial.println("1000 Hz"); //for testing
-  //   prev[1]=prev[2];
-  //   prev[2]=prev[3];
-  //   prev[3]=0;
-  //   if(!prev[1]||!prev[2]){
-  //     PORTC&=0x02;
-  //   }else{
-  //     PORTC&=~0x02;
-  //     prev[3]=1;
-  //   }
-  // }else{
-  //   PORTC&=~0x02;
-  //   prev[1]=prev[2];
-  //   prev[2]=prev[3];
-  //   prev[3]=1;
-  //   if(prev[1]||prev[2]){
-  //     PORTC&=~0x02;
-  //   }else{
-  //     PORTC&0x02;
-  //     prev[3]=0;
-  //   }
-  //   //it is 2000Hz
-  //   // Serial.println("2000 Hz");//for testing
+  // Serial.println("h4");
+  // for(int i=0;i<=7;i++){1
+  //   Serial.println("h5");
+  //   Serial.println(buff[i]);
   // }
-  // PORTC^=0x02;
+  vals[0]=buff[(2)&7]+(buff[(3)&7]<<1)+buff[(4)&7]-buff[(6)&7]-(buff[(7)&7]<<1)-buff[0];
+  vals[1]=(buff[(2)&7]<<1)-(buff[(4)&7]<<1)+(buff[(6)&7]<<1)-(buff[0]<<1);
+  vals[2]=(buff[(1)&7]<<1)+(buff[(2)&7])-buff[(4)&7]-(buff[(5)&7]<<1)-(buff[(6)&7])+buff[0];
+  vals[3]=-(buff[(2)&7]<<1)+(buff[(4)&7]<<1)-(buff[(6)&7]<<1)+(buff[0]<<1);
+  long freq_1000=vals[0]*vals[0]+vals[2]*vals[2];//is this correct?
+  long freq_2000=vals[1]*vals[1]+vals[3]*vals[3];
+  if(freq_1000>freq_2000){
+    PORTC|=0x04;
+    // not every sure why this doesn't work
+    // it should enter either if or else
+    //then it is 1000 Hz
+    // Serial.println("1000 Hz"); //for testing
+    // prev[1]=prev[2];
+    // prev[2]=prev[3];
+    // prev[3]=0;
+    // if(!prev[1]||!prev[2]){
+    //   // PORTC^=0x04;
+    //   PORTC&=0x04;
+    //   // sends low signal
+    // }else{
+    //   PORTC&=~0x04;
+    //   prev[3]=1;
+    // }
+  }else{
+    PORTC&=~0x04;
+    // prev[1]=prev[2];
+    // prev[2]=prev[3];
+    // prev[3]=1;
+    // if(prev[1]||prev[2]){
+    //   PORTC&=~0x04;
+    // }else{
+    //   PORTC&0x04;
+    //   prev[3]=0;
+    // }
+    //it is 2000Hz
+    // Serial.println("2000 Hz");//for testing
+  }
 }
